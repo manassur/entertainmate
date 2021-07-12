24ae4d41fa78ce729a85632870b534556a903f63
Manasseh <ologundemanasseh@gmail.com>
2021-07-12 11:21:16 +0100

    added create event provider

---
+++ lib/screens/utility/create_event_provider.dart
@@ -0,0 +1,86 @@
+import 'dart:convert';
+
+import 'package:entertainmate/screens/congrats.dart';
+import 'package:entertainmate/screens/model/generic_response.dart';
+import 'package:entertainmate/screens/model/user.dart';
+import 'package:entertainmate/screens/model/user.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import '../utility/constants.dart' as Constants;
+import 'package:entertainmate/screens/repository/repository.dart';
+import 'package:entertainmate/screens/verify_email//otp.dart';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'dart:io';
+import 'dart:math';
+
+import '../../mate.dart';
+
+class CreateEventProvider extends ChangeNotifier {
+  int eventType=0,categoryId=0,peopleCount=0;
+  String title='',description='',location='',startDate='',endDate='',audience='',categoryName='',peopleCountText='';
+  bool isLocationShown=false,isFirstInterestedAdded=false;
+
+  void setEventType(value) {
+    eventType = value;
+    notifyListeners();
+  }
+
+  void setCategoryId(value) {
+    categoryId  = value;
+    notifyListeners();
+  }
+
+  void setAudience(value) {
+   audience  = value;
+    notifyListeners();
+  }
+
+  void setPeopleCount(value) {
+    peopleCount= int.parse(value);
+    notifyListeners();
+  }
+
+  void setTitle(value) {
+   title = value;
+    notifyListeners();
+  }
+  void setDescription(value) {
+  description  = value;
+    notifyListeners();
+  }
+  void setLocation(value) {
+    location = value;
+    notifyListeners();
+  }
+  void setStartDate(value) {
+    startDate= value;
+    notifyListeners();
+  }
+  void setEndDate(value) {
+   endDate = value;
+    notifyListeners();
+  }
+
+  void setCategoryName(value) {
+   categoryName = value;
+    notifyListeners();
+  }
+
+  void setLocationShown(value) {
+    isLocationShown = value;
+    notifyListeners();
+  }
+
+  void setFirstInterestedAdded(value) {
+    isFirstInterestedAdded = value;
+    notifyListeners();
+  }
+
+
+//  bool isLoading() {
+//    return loading;
+//  }
+
+
+}
